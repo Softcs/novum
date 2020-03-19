1fd52ef48a1a446238f6f92da73f3124cff3f621
Daniel Jankowski (DAZJ) <dazj@netcompany.com>
2020-03-19 18:30:46 +0100

    Init

---
+++ src/app/_services/authentication.service.ts
@@ -0,0 +1,46 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { environment } from '@environments/environment';
+import { User } from '@app/_models';
+
+@Injectable({ providedIn: 'root' })
+export class AuthenticationService {
+    private currentUserSubject: BehaviorSubject<User>;
+    public currentUser: Observable<User>;
+
+    constructor(private http: HttpClient) {
+        this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
+        this.currentUser = this.currentUserSubject.asObservable();
+    }
+
+    public get currentUserValue(): User {
+        return this.currentUserSubject.value;
+    }
+
+    login(username: string, password: string) {
+        const userLocal = new User();
+        userLocal.password = password;
+        userLocal.username = username;
+        userLocal.authdata = window.btoa(username + ':' + password);
+        this.currentUserSubject.next(userLocal);
+
+        return this.http.post<any>(`${environment.apiUrl}/api/authentication/auth`, {  })
+            .pipe(map(user => {
+                // store user details and basic auth credentials in local storage to keep user logged in between page refreshes
+                user.authdata = window.btoa(username + ':' + password);
+                localStorage.setItem('currentUser', JSON.stringify(user));
+
+                this.currentUserSubject.next(user);
+                return user;
+            }));
+    }
+
+    logout() {
+        // remove user from local storage to log user out
+        localStorage.removeItem('currentUser');
+        this.currentUserSubject.next(null);
+    }
+}
