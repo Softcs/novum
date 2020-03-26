9373a66e9799fa51e1f8f423d8dcc1c18a8a625c
piotrr1975 <piotr.ratajczak@seidoit.pl>
2020-03-26 22:06:41 +0100

    Filtrowanie rozrachunków

--- src/app/containers/dictionaries/sitRozrachunkiInsertGT/sitRozrachunkiInsertGT.component.ts
+++ src/app/containers/dictionaries/sitRozrachunkiInsertGT/sitRozrachunkiInsertGT.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { sitSetDataSourceDirective, DataSourceContainerComponent } from '@app/components/data-source-container';
+import { MatTableDataSource, MatSort, MatPaginator } from '@angular/material';
+import * as $ from 'jquery';
 @Component({
   selector: 'sitRozrachunkiInsertGT',
   templateUrl: './sitRozrachunkiInsertGT.component.html',
@@ -8,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 export class SitRozrachunkiInsertGTComponent implements OnInit {
   displayedColumns: string[] = ['adr_Nazwa','nzf_NumerPelny','nzf_Data','nzf_TerminPlatnosci','DniSpoznienia','naleznosc','zobowiazanie'];
 
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(DataSourceContainerComponent, { static: true }) dataSourceContainer: DataSourceContainerComponent;
   constructor() { }
 
   ngOnInit() {
   }
-
+  applyFilter(event: Event,obj:any){
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSourceContainer.rows.filter = filterValue.trim().toLowerCase();
+  }
 }
