23b33cadd327ae610d960a4789c414640f35fcba
Daniel Jankowski (DAZJ) <dazj@netcompany.com>
2020-04-30 11:53:24 +0200

    dodanie crypt do proda

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://ws.seidoit.pl'
+    apiUrl: 'https://ws.seidoit.pl',
+    crypt: {
+        key: "72E93D2A56DB44C3914C811983C6C08E",
+        iv: "7061737323313233"
+    }
 };
