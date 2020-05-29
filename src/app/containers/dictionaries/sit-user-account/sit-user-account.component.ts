07e385c2e39ef371b2189c8e09dd9401307dd99c
piotrr1975 <piotr.ratajczak@seidoit.pl>
2020-05-29 07:09:03 +0200

    change zamiast onkeyup

--- src/app/containers/dictionaries/sit-user-account/sit-user-account.component.ts
+++ src/app/containers/dictionaries/sit-user-account/sit-user-account.component.ts
@@ -26,8 +26,12 @@ export class SitUserAccountComponent implements OnInit {
   }
 
   onKey(e) {
-    this.dictContainer.activeRow('sitProcGetJPKData')[e.srcElement.id] = e.srcElement.value;
-    console.log(this.dictContainer.activeRow('sitProcGetJPKData')[e.srcElement.id]);
+    this.dictContainer.activeRow('sitAppUserAccount')[e.srcElement.id] = e.srcElement.value;
   }
 
+  onChange(e) {
+    const dataSourceResponseWrapper: DataSourceResponseWrapper =
+      this.dictContainer.DataSourceManager.getDateSourceWrapper("sitAppUserAccount");
+    dataSourceResponseWrapper.activeRow[e.srcElement.id] = e.srcElement.value;
+  }
 }
