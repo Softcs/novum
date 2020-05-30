d5b75a62a8cbac311412405666a2eb6408857356
piotrr1975 <piotr.ratajczak@seidoit.pl>
2020-05-30 20:59:41 +0200

    Sprawdzanie

--- src/app/containers/dictionaries/sit-white-list-vat/sit-white-list-vat.component.ts
+++ src/app/containers/dictionaries/sit-white-list-vat/sit-white-list-vat.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { SitDictContainerComponent } from '@app/components/sit-dict-container';
+import { DataSourceResponseWrapper } from '@app/_models';
+
 
 @Component({
   selector: 'app-sit-white-list-vat',
@@ -7,10 +10,20 @@ import { Component, OnInit } from '@angular/core';
   host: {class: 'router-flex'}
 })
 export class SitWhiteListVATComponent implements OnInit {
+  @ViewChild('sitDictcontainer') dictContainer: SitDictContainerComponent;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  get activeRow() {
+    return this.dictContainer?.activeRow('sitJPKCheckVatNIPNRB');
+  }
+
+  onChange(e) {
+    const dataSourceResponseWrapper: DataSourceResponseWrapper =
+      this.dictContainer.DataSourceManager.getDateSourceWrapper("sitJPKCheckVatNIPNRB");
+    dataSourceResponseWrapper.activeRow[e.srcElement.id] = e.srcElement.value;
+  }
 }
