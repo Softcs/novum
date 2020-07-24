e332656f8090aafcd263ab5511cc4ba5b31d78c8
piotrr1975 <piotr.ratajczak@seidoit.pl>
2020-07-24 11:22:05 +0200

    Powrót do pierwszej wersji zakładek

--- src/app/_models/tab.model.ts
+++ src/app/_models/tab.model.ts
@@ -1,19 +1,13 @@
 import { Type } from '@angular/core';
 export class Tab {
   public id: number;
-  public ident: string;
   public title: string;
   public tabData: any;
   public active: boolean;
-  //public component: Type<any>;
-  public link: string;
-
-//  constructor(component: Type<any>, link: string, title: string, tabData: any) {
-  constructor(ident: string, link: string, title: string, tabData: any) {
-    this.tabData = tabData;
-    this.title = title;
-    this.link = link;
-    this.active = true;
-    this.ident = ident;
+  public component: Type<any>;
+  constructor(component: Type<any>, title: string, tabData: any) {
+  this.tabData = tabData;
+  this.component = component;
+  this.title = title;
   }
 }
--- src/app/_services/tab.service.ts
+++ src/app/_services/tab.service.ts
@@ -1,81 +1,45 @@
-import { Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { Tab } from '@app/_models/tab.model';
 import { SitPulpitComponent } from '@app/containers/sit-pulpit';
 
 import { BehaviorSubject } from 'rxjs';
-import { TabData } from '@app/_models/tabdata';
-import { DataSetManager } from '@app/_models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TabService {
-  activeTabIndex: number;
-
-  constructor(
-    public router: Router
-  ) { }
 
   public tabs: Tab[] = [
-    new Tab('sitPulpit', 'sitPulpit', 'Pulpit', { parent: 'AppComponent' }),
+    new Tab(SitPulpitComponent, 'Pulpit', { parent: 'AppComponent' }),
     ];
 
   public tabSub = new BehaviorSubject<Tab[]>(this.tabs);
 
   public removeTab(index: number) {
-    var wasActive = this.tabs[this.activeTabIndex].active;
-    console.log('removeTab', index, this.tabs, this.tabs[index - 1].link)
-
     this.tabs.splice(index, 1);
-    if (wasActive) {
-      this.changeTab(index - 1);
+    if (this.tabs.length > 0) {
+    this.tabs[this.tabs.length - 1].active = true;
     }
-  }
-
-  private getTabIdent(): string {
-    const tab = this.tabs[this.activeTabIndex];
-    return tab?.tabData['tabIdent'];
+    this.tabSub.next(this.tabs);
   }
 
   public changeTab(index: number) {
-    this.activeTabIndex = this.tabs.findIndex(t => t.active);
-    const lastActiveTab = this.tabs[this.activeTabIndex];
-    if (lastActiveTab != null) {
-      lastActiveTab.active = false;
+    for (let i = 0; i < this.tabs.length; i++) {
+      if (index !== i ) { this.tabs[i].active = false; } else { this.tabs[i].active = true; }
+      this.tabSub.next(this.tabs);
     }
-
-    this.tabs[index].active = true;
-    this.tabSub.next(this.tabs);
-    this.activeTabIndex = this.tabs.findIndex(t => t.active);
-    const linkToNavigate = this.tabs[this.activeTabIndex].link;
-    const tabIdent = this.getTabIdent();
-    tabIdent == null ? this.router.navigate([linkToNavigate]) : this.router.navigate([linkToNavigate, tabIdent]);
-
   }
 
   public addTab(tab: Tab) {
-    this.activeTabIndex = this.tabs.findIndex(t => t.active);
-    const tabIndex = this.tabs.findIndex(t => t.ident === tab.ident);
+    for (let i = 0; i < this.tabs.length; i++) {
+    if (this.tabs[i].active === true) {
+    this.tabs[i].active = false;
+    }}
+    tab.id = this.tabs.length + 1;
+    tab.active = true;
+    this.tabs.push(tab);
+    this.tabSub.next(this.tabs);
+    }
 
-    if (tabIndex !== -1) {
-      console.log('Tabs po przelaczeniu', this.tabs, this.tabs[this.activeTabIndex].link);
-      this.changeTab(tabIndex);
-    } else {
-      console.log('Tabs po utworzeniu nowego', this.tabs);
-      this.tabs[this.activeTabIndex].active = false;
-      tab.id = this.tabs.length + 1;
-      tab.active = true;
-      this.tabs.push(tab);
-      this.tabSub.next(this.tabs);
-      this.activeTabIndex = this.tabs.findIndex(t => t.active);
-      const tabIdent = this.getTabIdent();
-      if (tabIdent == null) {
-        this.router.navigate([this.tabs[this.activeTabIndex].link]);
-      }
-      else {
-        this.router.navigate([this.tabs[this.activeTabIndex].link, tabIdent]);
-      }
-      }
-  }
+  constructor() { }
 }
