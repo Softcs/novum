0219635d1059ccb66f5cfb5f7ff186dbe7be63e0
piotrr1975 <piotr.ratajczak@seidoit.pl>
2020-07-28 10:10:41 +0200

    Poprawa po merge 2

--- src/app/containers/dictionaries/sit-jpk-vat/actions/sit-jpk-vat-get-data/sit-jpk-vat-get-data.component.ts
+++ src/app/containers/dictionaries/sit-jpk-vat/actions/sit-jpk-vat-get-data/sit-jpk-vat-get-data.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { ActionExecuteData } from '@app/_models/actionExecuteData';
+import { SitActionParamsForm } from '@app/_interfaces/sitActionParamsForm';
 @Component({
   selector: 'app-sit-jpk-vat-get-data',
   templateUrl: './sit-jpk-vat-get-data.component.html',
   styleUrls: ['./sit-jpk-vat-get-data.component.scss']
 })
-export class SitJpkVatGetDataComponent implements OnInit {
+export class SitJpkVatGetDataComponent implements SitActionParamsForm, OnInit {
+  @Input() actionExecuteData: ActionExecuteData;
 
   constructor() { }
 
