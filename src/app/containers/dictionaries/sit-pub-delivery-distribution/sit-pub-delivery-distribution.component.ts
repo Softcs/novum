cac77483ba899b6357d5ce98fab48409ade2b98d
Piotr Ratajczak <piotr.ratajczak@seidoit.pl>
2021-01-25 16:15:56 +0100

    Formatowanie eksportu do Excela - cellClass, excelStyles

--- src/app/containers/dictionaries/sit-pub-delivery-distribution/sit-pub-delivery-distribution.component.ts
+++ src/app/containers/dictionaries/sit-pub-delivery-distribution/sit-pub-delivery-distribution.component.ts
@@ -23,6 +23,7 @@ export class SitPubDeliveryDistributionComponent implements OnInit {
   popupParent;
   defaultColDef;
   columnDefs;
+  excelStyles;
 
   constructor(
     private gatewayService: GatewayService,
@@ -31,40 +32,124 @@ export class SitPubDeliveryDistributionComponent implements OnInit {
   ) {
     this.gatewayService.currentUser.subscribe(x => this.currentUser = x);
     this.popupParent = document.querySelector('body');
+    this.excelStyles = [
+      {
+        id: 'header',
+        font: {
+            fontName: "Calibri",
+            size: 12,
+            bold: true
+        },
+        interior: {
+          color: '#ebebe0',
+          pattern: 'Solid',
+        },
+      },
+      {
+        id: 'font12',
+        font: { fontName: "Calibri", size: 12, }
+      },
+      {
+        id: 'textFormat',
+        dataType: 'string',
+      },
+      {
+        id: 'blueBackground',
+        interior: {
+          color: '#cce6ff',
+          pattern: 'Solid',
+        },
+      },
+      {
+        id: 'pinkBackground',
+        interior: {
+          color: '#ffe6e6',
+          pattern: 'Solid',
+        },
+      },
+      {
+        id: 'greenBackground',
+        interior: {
+          color: '#d6f5d6',
+          pattern: 'Solid',
+        },
+      },
+      {
+        id: 'dateFormat',
+        dataType: 'dateTime',
+        numberFormat: { format: 'yyyy-mm-dd;@' },
+      },
+      {
+        id: 'numberFormatInt',
+        numberFormat: { format: '#,##0' },
+      },
+      {
+        id: 'numberFormat2Dec',
+        numberFormat: { format: '#,##0.00' },
+      },
+      {
+        id: 'numberFormatPercent',
+        numberFormat: { format: '#,##0[$%]' },
+      },
+      {
+        id: 'textFormat',
+        dataType: 'string',
+      },
+    ]
 
     this.columnDefs = [
       { headerName: 'Kartoteka',
         children: [
-          { headerName: 'EAN', field: 'EAN', filter: 'agTextColumnFilter', width: 120, pinned: 'left' },
-          { headerName: 'Status', headerTooltip: 'Premiera, Nowość, Dodruk', field: 'ProjectStatus', filter: 'agTextColumnFilter', width: 70, pinned: 'left', enableRowGroup: true, },
-          { headerName: 'Opis', field: 'ProductName', tooltipField: 'ProductName', filter: 'agTextColumnFilter', width: 200, pinned: 'left' },
+          { headerName: 'EAN', field: 'EAN', filter: 'agTextColumnFilter', width: 120, pinned: 'left',
+            cellClass: ['font12','textFormat']
+          },
+          { headerName: 'Status', headerTooltip: 'Premiera, Nowość, Dodruk', field: 'ProjectStatus', filter: 'agTextColumnFilter', width: 70, pinned: 'left', enableRowGroup: true,
+            cellClass: ['font12','textFormat']
+          },
+          { headerName: 'Opis', field: 'ProductName', tooltipField: 'ProductName', filter: 'agTextColumnFilter', width: 200, pinned: 'left',
+            cellClass: ['font12','textFormat']
+          },
         ]
       },
-      { headerName: 'Wyd.', field: 'Publishing', filter: 'agTextColumnFilter', width: 80, enableRowGroup: true, },
-      { headerName: 'Rynki', field: 'Markets',tooltipField: 'Markets' , filter: 'agTextColumnFilter', width: 80, enableRowGroup: true, },
+      { headerName: 'Wyd.', field: 'Publishing', filter: 'agTextColumnFilter', width: 80, enableRowGroup: true,
+        cellClass: ['font12','textFormat'] },
+      { headerName: 'Rynki', field: 'Markets',tooltipField: 'Markets' , filter: 'agTextColumnFilter', width: 80, enableRowGroup: true,
+        cellClass: ['font12','textFormat']
+      },
       { headerName: 'Data wyd', field: 'ReleaseDateAct', filter: 'agDateColumnFilter', width: 100, enableRowGroup: true,
-        cellRenderer: function(params) { return formatDate(params.value, 'yyyy-MM-dd', locale) }
+        cellRenderer: function(params) { return formatDate(params.value, 'yyyy-MM-dd', locale) },
+        cellClass: ['font12','dateFormat']
       },
       { headerName: 'Ceny',
         children: [
           { headerName: 'Detal.', field: 'DetailPrice', type: 'rightAligned', filter: 'agNumberColumnFilter', autoHeight: true, width: 90,
-            cellRenderer: function(params) { return formatNumber(params.value, locale,'1.2-2') }
+            cellRenderer: function(params) { return formatNumber(params.value, locale,'1.2-2') },
+            cellClass: ['font12','numberFormat2Dec']
           },
           { headerName: 'Zakupu', field: 'PurchacePrice', type: 'rightAligned', filter: 'agNumberColumnFilter', autoHeight: true, width: 90,
-            cellRenderer: function(params) { return formatNumber(params.value, locale,'1.2-2') }
+            cellRenderer: function(params) { return formatNumber(params.value, locale,'1.2-2') },
+            cellClass: ['font12','numberFormat2Dec']
           },
         ]
       },
       { headerName: 'Dostawy', field: 'DeliveryQuantity', type: 'numericColumn', filter: 'agNumberColumnFilter', autoHeight: true, width: 100, enableValue: true,
+        cellClass: ['font12','numberFormatInt']
       },
       { headerName: 'Sprzedaż', field: 'SaleQuantity', type: 'rightAligned', filter: 'agNumberColumnFilter', autoHeight: true, width: 100, enableValue: true,
+        cellClass: ['font12','numberFormatInt']
       },
       { headerName: '% dost.', field: 'DeliveryPercent', type: 'rightAligned', filter: 'agNumberColumnFilter', autoHeight: true, width: 90,
-        cellRenderer: function(params) { return formatNumber(params.value, locale,'1.0-0')+'%' }
+        cellRenderer: function(params) { return formatNumber(params.value, locale,'1.0-0')+'%' },
+        cellClass: ['font12','numberFormatPercent']
       },
-      { headerName: 'Stan ogółem', field: 'AllStock', type: 'rightAligned', filter: 'agNumberColumnFilter', autoHeight: true, width: 110, enableValue: true, },
-      { headerName: 'Stan A100', field: 'WMSStock', type: 'rightAligned', filter: 'agNumberColumnFilter', autoHeight: true, width: 110, enableValue: true, },
+      { headerName: 'Stan ogółem', field: 'AllStock', type: 'rightAligned', filter: 'agNumberColumnFilter', autoHeight: true, width: 110, enableValue: true,
+        cellClass: ['font12','numberFormatInt']
+      },
+      { headerName: 'Stan A100', field: 'WMSStock', type: 'rightAligned', filter: 'agNumberColumnFilter', autoHeight: true, width: 110, enableValue: true,
+        cellClass: ['font12','numberFormatInt']
+      },
       { headerName: 'Stan dysp.', field: 'WMSStockAv', type: 'rightAligned', filter: 'agNumberColumnFilter', autoHeight: true, width: 100, enableValue: true,
+        cellClass: ['font12','numberFormatInt']
       },
       // { headerName: 'OtherStock', field: 'OtherStock', type: 'rightAligned', filter: 'agNumberColumnFilter', autoHeight: true, width: 100,
       //   cellRenderer: function(params) { return formatNumber(params.value, locale,'1.2-2') }
@@ -72,86 +157,104 @@ export class SitPubDeliveryDistributionComponent implements OnInit {
       { headerName: 'Rozdysponowano',
         children: [
           { headerName: 'Ilość', field: 'DistributedQuantity', type: 'rightAligned', filter: 'agNumberColumnFilter', autoHeight: true, width: 90, enableValue: true,
-            cellStyle: function(params) { return {backgroundColor: '#cce6ff'} }
+            cellStyle: function(params) { return {backgroundColor: '#cce6ff'} },
+            cellClass: ['font12','blueBackground','numberFormatInt'],
           },
           { headerName: '% dost.', field: 'DistributedPercent', type: 'rightAligned', filter: 'agNumberColumnFilter', autoHeight: true, width: 90,
             cellRenderer: function(params) { return formatNumber(params.value, locale,'1.0-0')+'%' },
-            cellStyle: function(params) { return {backgroundColor: '#cce6ff'} }
+            cellStyle: function(params) { return {backgroundColor: '#cce6ff'} },
+            cellClass: ['font12','blueBackground','numberFormatPercent'],
           },
         ]
       },
       { headerName: 'Konsygnacja',
         children: [
           { headerName: 'Ilość', field: 'ConsignmentQuantitySum', type: 'rightAligned', filter: 'agNumberColumnFilter', autoHeight: true, width: 90, enableValue: true,
-            cellStyle: function(params) { return {backgroundColor: '#ffe6e6'} }
+            cellStyle: function(params) { return {backgroundColor: '#ffe6e6'} },
+            cellClass: ['font12','pinkBackground','numberFormatInt'],
           },
           { headerName: '% rozdysp.', field: 'ConsignmentPercent', type: 'rightAligned', filter: 'agNumberColumnFilter', autoHeight: true, width: 90,
             cellRenderer: function(params) { return formatNumber(params.value, locale,'1.0-0')+'%' },
-            cellStyle: function(params) { return {backgroundColor: '#ffe6e6'} }
+            cellStyle: function(params) { return {backgroundColor: '#ffe6e6'} },
+            cellClass: ['font12','pinkBackground',,'numberFormatPercent'],
           },
         ]
       },
       { headerName: 'Sprzedaż',
         children: [
           { headerName: 'Ilość', field: 'SaleQuantitySum', type: 'rightAligned', filter: 'agNumberColumnFilter', autoHeight: true, width: 90, enableValue: true,
-            cellStyle: function(params) { return {backgroundColor: '#d6f5d6'} }
-
+            cellStyle: function(params) { return {backgroundColor: '#d6f5d6'} },
+            cellClass: ['font12','greenBackground','numberFormatInt'],
           },
           { headerName: '% rozdysp.', field: 'SalePercent', type: 'rightAligned', filter: 'agNumberColumnFilter', autoHeight: true, width: 90,
             cellRenderer: function(params) { return formatNumber(params.value, locale,'1.0-0')+'%' },
-            cellStyle: function(params) { return {backgroundColor: '#d6f5d6'} }
-
+            cellStyle: function(params) { return {backgroundColor: '#d6f5d6'} },
+            cellClass: ['font12','greenBackground',,'numberFormatPercent'],
           },
         ]
       },
       { headerName: 'Hania',
         children: [
           { headerName: 'Konsygn.', field: 'HConsignmentQuantity', type: 'rightAligned', filter: 'agNumberColumnFilter', autoHeight: true, width: 100,
+            cellClass: ['font12','numberFormatInt'],
           },
           { headerName: 'Sprzedaż', field: 'HSaleQuantity', type: 'rightAligned', filter: 'agNumberColumnFilter', autoHeight: true, width: 100,
+            cellClass: ['font12','numberFormatInt',],
           },
           { headerName: 'PZR', field: 'HPZRQuantity', type: 'rightAligned', filter: 'agNumberColumnFilter', autoHeight: true, width: 100,
-            cellStyle: function(params) { return {backgroundColor: '#ffe6e6'} }
+            cellStyle: function(params) { return {backgroundColor: '#ffe6e6'} },
+            cellClass: ['font12','pinkBackground','numberFormatInt'],
           },
           { headerName: '%', field: 'HPercent', type: 'rightAligned', filter: 'agNumberColumnFilter', autoHeight: true, width: 80,
             cellRenderer: function(params) { return formatNumber(params.value, locale,'1.0-0')+'%' },
-            cellStyle: function(params) { return {backgroundColor: '#ffe6e6'} }
+            cellStyle: function(params) { return {backgroundColor: '#ffe6e6'} },
+            cellClass: ['font12','pinkBackground','numberFormatPercent'],
           },
         ]
       },
       { headerName: 'Zbyszek',
         children: [
           { headerName: 'Konsygn.', field: 'ZConsignmentQuantity', type: 'rightAligned', filter: 'agNumberColumnFilter', autoHeight: true, width: 100,
+            cellClass: ['font12','numberFormatInt'],
           },
           { headerName: 'Sprzedaż', field: 'ZSaleQuantity', type: 'rightAligned', filter: 'agNumberColumnFilter', autoHeight: true, width: 100,
+          cellClass: ['font12','numberFormatInt'],
           },
           { headerName: 'PZR', field: 'ZPZRQuantity', type: 'rightAligned', filter: 'agNumberColumnFilter', autoHeight: true, width: 100,
-            cellStyle: function(params) { return {backgroundColor: '#ffe6e6'} }
+            cellStyle: function(params) { return {backgroundColor: '#ffe6e6'} },
+            cellClass: ['font12','pinkBackground','numberFormatInt'],
           },
           { headerName: '%', field: 'ZPercent', type: 'rightAligned', filter: 'agNumberColumnFilter', autoHeight: true, width: 80,
             cellRenderer: function(params) { return formatNumber(params.value, locale,'1.0-0')+'%' },
-            cellStyle: function(params) { return {backgroundColor: '#ffe6e6'} }
+            cellStyle: function(params) { return {backgroundColor: '#ffe6e6'} },
+            cellClass: ['font12','pinkBackground','numberFormatPercent'],
           },
         ]
       },
       { headerName: 'Dorota',
         children: [
           { headerName: 'Konsygn.', field: 'DConsignmentQuantity', type: 'rightAligned', filter: 'agNumberColumnFilter', autoHeight: true, width: 100,
+            cellClass: ['font12','numberFormatInt'],
           },
           { headerName: 'Sprzedaż', field: 'DSaleQuantity', type: 'rightAligned', filter: 'agNumberColumnFilter', autoHeight: true, width: 100,
+            cellClass: ['font12','numberFormatInt'],
           },
           { headerName: 'PZR', field: 'DPZRQuantity', type: 'rightAligned', filter: 'agNumberColumnFilter', autoHeight: true, width: 100,
-            cellStyle: function(params) { return {backgroundColor: '#ffe6e6'} }
+            cellStyle: function(params) { return {backgroundColor: '#ffe6e6'} },
+            cellClass: ['font12','pinkBackground','numberFormatInt'],
           },
           { headerName: '%', field: 'DPercent', type: 'rightAligned', filter: 'agNumberColumnFilter', autoHeight: true, width: 80,
             cellRenderer: function(params) { return formatNumber(params.value, locale,'1.0-0')+'%' },
-            cellStyle: function(params) { return {backgroundColor: '#ffe6e6'} }
+            cellStyle: function(params) { return {backgroundColor: '#ffe6e6'} },
+            cellClass: ['font12','pinkBackground','numberFormatPercent'],
           },
         ]
       },
       { headerName: 'Pozostałe',
         children: [
-          { headerName: 'Sprzedaż', field: 'OSaleQuantity', type: 'rightAligned', filter: 'agNumberColumnFilter', autoHeight: true, width: 100 },
+          { headerName: 'Sprzedaż', field: 'OSaleQuantity', type: 'rightAligned', filter: 'agNumberColumnFilter', autoHeight: true, width: 100,
+            cellClass: ['font12','numberFormatInt'],
+           },
         ]
       }
     ]
