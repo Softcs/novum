7e5860f808cfaa5cf9f52a82e4d6747565672d6d
Marcin Patyna <mpatyna@gmail.com>
2021-03-05 16:00:52 +0100

    Dodanie komponentu sit-documents-types

--- src/app/containers/dictionaries/sit-documents-types/actions/sit-documents-types-edit/sit-documents-types-edit.component.ts
+++ src/app/containers/dictionaries/sit-documents-types/actions/sit-documents-types-edit/sit-documents-types-edit.component.ts
@@ -1,15 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { SitActionParamsForm } from '@app/_interfaces/sitActionParamsForm';
 
 @Component({
   selector: 'app-sit-documents-types-edit',
   templateUrl: './sit-documents-types-edit.component.html',
   styleUrls: ['./sit-documents-types-edit.component.scss']
 })
-export class SitDocumentsTypesEditComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class SitDocumentsTypesEditComponent extends SitActionParamsForm  {
 
 }
--- src/app/containers/dictionaries/sit-documents-types/sit-documents-types.component.ts
+++ src/app/containers/dictionaries/sit-documents-types/sit-documents-types.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '@app/_models';
+import { GatewayService } from '@app/_services';
+import { GridService } from '@app/_services/grid.service';
 
 @Component({
   selector: 'app-sit-documents-types',
   templateUrl: './sit-documents-types.component.html',
-  styleUrls: ['./sit-documents-types.component.scss']
+  styleUrls: ['./sit-documents-types.component.scss'],
+  host: {class: 'router-flex'}
 })
-export class SitDocumentsTypesComponent implements OnInit {
+export class SitDocumentsTypesComponent {
+  currentUser: User;
+  popupParent;
+  columnDefs;
 
-  constructor() { }
+  constructor(
+    private gatewayService: GatewayService,
+    private gridService: GridService
+  ) {
+    this.gatewayService.currentUser.subscribe(x => this.currentUser = x);
+    this.popupParent = document.querySelector('body');
 
-  ngOnInit(): void {
+    this.columnDefs = [
+      { headerName: 'Identyfikator', field: 'DocumentIdent', filter: 'agTextColumnFilter', width: 150 },
+      { headerName: 'Nazwa', field: 'DocumentName', filter: 'agTextColumnFilter', width: 300 },
+      { headerName: 'Opis', field: 'DocumentDescription', filter: 'agTextColumnFilter', width: 300 },
+      { headerName: 'Wpływ na magazyn', field: 'StockEffect', type: "numericColumn" },
+    ]
+
+   }
+
+   onGridReady(params) {
+    this.gridService.setDefGridOptionsOnReady(params);
   }
 
 }
