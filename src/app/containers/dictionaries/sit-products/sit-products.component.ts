42f09b7a9ced25d32f3daa5e8ec47fb035a8c155
Daniel Jankowski <daniel.jankowski@softcs.eu>
2021-03-10 08:49:51 +0100

    poprawki sit-products

--- src/app/containers/dictionaries/sit-products/sit-products.component.ts
+++ src/app/containers/dictionaries/sit-products/sit-products.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Inject, LOCALE_ID } from '@angular/core';
 import { SitDictBaseComponent } from '@app/containers/_base/sit-dict-base/sit-dict-base.component';
+import { GatewayService } from '@app/_services';
+import { AttachmentsService } from '@app/_services/attachments.service';
+import { GridService } from '@app/_services/grid.service';
 
 
 @Component({
@@ -9,30 +12,47 @@ import { SitDictBaseComponent } from '@app/containers/_base/sit-dict-base/sit-di
   host: {class: 'router-flex'}
 })
 export class SitProductsComponent extends SitDictBaseComponent {
+  
+  link;
+  ean;
+  contentColor;
+  
+  constructor(
+    protected gatewayService: GatewayService,
+    private attachmentsService: AttachmentsService,
+    protected gridService: GridService,
+    @Inject(LOCALE_ID) protected locale: string
+  ) {
+    super(gatewayService, gridService, locale);
+
+    this.contentColor = document.documentElement.style.getPropertyValue('$content-background-color');
+    this.popupParent = document.querySelector('body');
+
+  }
   public prepareColumnsDefinitnion() {   
-  this.gridColumnsDefinition["sitProducts"] = [
-    { headerName: 'Identyfikator', field: 'ProductIdent', filter: 'agTextColumnFilter', width: 150, },
-    { headerName: 'EAN', field: 'EAN', filter: 'agTextColumnFilter', width: 120 },
-    { headerName: 'Nazwa', field: 'ProductName', tooltipField: 'ProductName', filter: 'agTextColumnFilter', width: 300 },
-    { headerName: 'JM', field: 'UnitIdent', filter: 'agTextColumnFilter', width: 60 },
-    { headerName: 'Vat', field: 'VATRateIdent', filter: 'agTextColumnFilter', width: 60 },
-    { headerName: 'PKWIU', field: 'PKWIU', filter: 'agTextColumnFilter', width: 100 },
-    { headerName: 'Waga kg', field: 'Weight', filter: 'agTextColumnFilter', type: 'numericColumn', width: 90,
-    renderType: "number", renderFormat: '1.3-3'
-    },
-    { headerName: 'Aktywny', field: 'IsActive', filter: 'agSetColumnFilter', width: 80, cellRenderer: 'gridCheckboxRenderer', suppressMenu: true },
-    { headerName: 'Status sprz.', field: 'SaleStatus', tooltipField: 'SaleStatusDescription', width: 80, suppressMenu: true},
+    this.gridColumnsDefinition["sitProducts"] = [
+      { headerName: 'Identyfikator', field: 'ProductIdent', filter: 'agTextColumnFilter', width: 150, },
+      { headerName: 'EAN', field: 'EAN', filter: 'agTextColumnFilter', width: 120 },
+      { headerName: 'Nazwa', field: 'ProductName', tooltipField: 'ProductName', filter: 'agTextColumnFilter', width: 300 },
+      { headerName: 'JM', field: 'UnitIdent', filter: 'agTextColumnFilter', width: 60 },
+      { headerName: 'Vat', field: 'VATRateIdent', filter: 'agTextColumnFilter', width: 60 },
+      { headerName: 'PKWIU', field: 'PKWIU', filter: 'agTextColumnFilter', width: 100 },
+      { headerName: 'Waga kg', field: 'Weight', filter: 'agTextColumnFilter', type: 'numericColumn', width: 90,
+      renderType: "number", renderFormat: '1.3-3'
+      },
+      { headerName: 'Aktywny', field: 'IsActive', filter: 'agSetColumnFilter', width: 80, cellRenderer: 'gridCheckboxRenderer', suppressMenu: true },
+      { headerName: 'Status sprz.', field: 'SaleStatus', tooltipField: 'SaleStatusDescription', width: 80, suppressMenu: true},
 
 
-  ];
-  this.gridColumnsDefinition["sitProductSaleStatusIntervals"] = [
-    { headerName: 'Od dnia', field: 'DateFrom', width: 100,},
-    { headerName: 'Status', field: 'SaleStatus', width: 80,},
-    { headerName: 'Opis', field: 'SaleStatusDescription', width: 150,},
-  ]
-}
+    ];
+    this.gridColumnsDefinition["sitProductSaleStatusIntervals"] = [
+      { headerName: 'Od dnia', field: 'DateFrom', width: 100,},
+      { headerName: 'Status', field: 'SaleStatus', width: 80,},
+      { headerName: 'Opis', field: 'SaleStatusDescription', width: 150,},
+    ]
+  }
 
-    activeRowProductsChanged(activeRow) {
+  activeRowProductsChanged(activeRow) {
     this.link = activeRow?.sitImagesG == null
       ? this.attachmentsService.getUrl(this.currentUser, "noimage", "noimage.jpg") // kiedy brak rekordu
       :  this.attachmentsService.getUrl(this.currentUser, activeRow.sitImagesG, activeRow.FileName) ;
