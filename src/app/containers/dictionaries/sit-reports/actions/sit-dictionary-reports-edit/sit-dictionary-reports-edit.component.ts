8358608f6f0f8f0bdc0f00449633c1e121a92ee1
radoslawmotak <radoslaw.motak@seidoit.pl>
2021-03-21 20:05:52 +0100

    szczegóły do sitReports

---
+++ src/app/containers/dictionaries/sit-reports/actions/sit-dictionary-reports-edit/sit-dictionary-reports-edit.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { SitActionParamsForm } from '@app/_interfaces/sitActionParamsForm';
+
+@Component({
+  selector: 'app-sit-dictionary-reports-edit',
+  templateUrl: './sit-dictionary-reports-edit.component.html',
+  styleUrls: ['./sit-dictionary-reports-edit.component.scss']
+})
+export class SitDictionaryReportsEditComponent extends SitActionParamsForm  {
+
+}
--- src/app/containers/dictionaries/sit-reports/sit-reports.component.ts
+++ src/app/containers/dictionaries/sit-reports/sit-reports.component.ts
@@ -14,6 +14,17 @@ export class SitReportsComponent extends SitDictBaseComponent {
       { headerName: 'Identyfikator', field: 'ReportIdent', filter: 'agTextColumnFilter', width: 150 },
       { headerName: 'Nazwa', field: 'ReportName', filter: 'agTextColumnFilter', width: 300 },
       { headerName: 'Plik wydruku', field: 'ReportFileName', filter: 'agTextColumnFilter', width: 300 }
+    ];
+    this.gridColumnsDefinition["sitDocumentsTypesReports"] = [
+      { headerName: 'Dokument', field: 'DocumentIdent', width: 150,},
+      { headerName: 'Opis dokumentu', field: 'DocumentName', width: 300,},
+      { headerName: 'Domyślny', field: 'IsDefault', width: 100, renderType: 'checkbox'}
+    ];
+    this.gridColumnsDefinition["sitDictionaryReports"] = [
+      { headerName: 'Słownik', field: 'DictionaryIdent', width: 150 },
+      { headerName: 'Opis słownika', field: 'DictionaryName', width: 300 },
+      { headerName: 'Kolejność', field: 'Order', width: 100 },
+      { headerName: 'Domyślny', field: 'IsDefault', width: 100, renderType: 'checkbox'}
     ]
    }
 }
