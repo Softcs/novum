7e8ef34f3f0d9e2818b91d9a83fa462143406be0
Piotr Ratajczak <piotr.ratajczak@seidoit.pl>
2021-05-12 11:28:59 +0200

    Wydruk wniosku urlopowego

--- src/app/containers/dictionaries/sit-vacation-requests/sit-vacation-requests.component.ts
+++ src/app/containers/dictionaries/sit-vacation-requests/sit-vacation-requests.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { SitDictBaseComponent } from '@app/containers/_base/sit-dict-base/sit-dict-base.component';
+import { DataSetWrapper } from '@app/_models/dataSetWrapper';
 
 @Component({
   selector: 'app-sit-vacation-requests',
@@ -30,5 +31,18 @@ export class SitVacationRequestsComponent  extends SitDictBaseComponent {
       },
       { headerName: 'Opis', field: 'Description', tooltipField: 'Description', filter: 'agTextColumnFilter', wrapText: true, width: 250},
     ];
+  }
+
+  getPrintout() {
+    //generuje wydruk w nowej zakładce
+    const dataSourceResponseWrapper: DataSetWrapper = this.dictContainer.DataSetManager.getDateSourceWrapper('sitVacationRequests');
+    var url = this.urlService.getSecureRepUrl(
+        '70F1D169-5210-C62A-675F-AD0B0DD3CA46',
+        'dict',
+        '9069CE97-3F9D-29A4-BD1D-321C12B51E49',
+        'sitVacationRequestsId=' + dataSourceResponseWrapper.activeRow['sitVacationRequestsId']);
+
+    window.open(url, '','height=700, width=1400, left=100,top=100');
+
   }
 }
