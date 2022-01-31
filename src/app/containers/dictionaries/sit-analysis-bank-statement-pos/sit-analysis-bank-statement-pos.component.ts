062b5ee03c7b3123a55ec1eaf7130c5efdb46644
Daniel Jankowski <daniel.jankowski@softcs.eu>
2022-01-31 06:45:37 +0100

    analizy - pivot - wlaczenie konfiguracji, enableRowGroup: true

--- src/app/containers/dictionaries/sit-analysis-bank-statement-pos/sit-analysis-bank-statement-pos.component.ts
+++ src/app/containers/dictionaries/sit-analysis-bank-statement-pos/sit-analysis-bank-statement-pos.component.ts
@@ -8,26 +8,26 @@ import { SitDictBaseComponent } from '@app/containers/_base/sit-dict-base/sit-di
   host: {class: 'router-flex'}
 })
 export class SitAnalysisBankStatementPosComponent extends SitDictBaseComponent {
-  
+
   public prepareColumnsDefinitnion(){
     this.gridColumnsDefinition["sitAnalysisBankStatementPos"] = [
       { headerName: 'Bank', field: 'BankStatementIdent', filter: 'agTextColumnFilter', width: 100,
-        rowGroup: "true", enableRowGroup: "true", enablePivot: "true" },
+        rowGroup: true, enableRowGroup: true, enablePivot: true},
       { headerName: 'Kontrahent', field: 'CustIdent', filter: 'agTextColumnFilter', width: 100,
-        rowGroup: "true", enableRowGroup: "true", enablePivot: "true" },
+        rowGroup: true, enableRowGroup: true, enablePivot: true },
       { headerName: 'Nazwa', field: 'CustName', filter: 'agTextColumnFilter', width: 100 },
       { headerName: 'Data', field: 'BankStatementDate', filter: 'agTextColumnFilter', width: 100 },
       { headerName: 'Rok', field: 'Year', filter: 'agTextColumnFilter', width: 100,
-        pivot: "true", enablePivot: "true" },  
+        pivot: true, enablePivot: true },
       { headerName: 'Rok-miesiąc', field: 'YearMonth', filter: 'agTextColumnFilter', width: 100,
-        pivot: "true", enablePivot: "true"  },  
-      { headerName: 'Kwota', field: 'Amount', filter: 'agTextColumnFilter', width: 100, renderType: "number", type: 'numericColumn', 
-        aggFunc: "sum"},          
+        pivot: true, enablePivot: true  },
+      { headerName: 'Kwota', field: 'Amount', filter: 'agTextColumnFilter', width: 100, renderType: "number", type: 'numericColumn',
+        aggFunc: "sum"},
       { headerName: 'Kwota +', field: 'AmountIn', filter: 'agTextColumnFilter', width: 100, renderType: "number", type: 'numericColumn',
-        aggFunc: "sum"},          
+        aggFunc: "sum"},
       { headerName: 'Kwota -', field: 'AmountOut', filter: 'agTextColumnFilter', width: 100, renderType: "number", type: 'numericColumn',
-        aggFunc: "sum"},          
+        aggFunc: "sum"},
     ];
- 
+
    }
 }
