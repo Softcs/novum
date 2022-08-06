b9256825d131588108a6d024211d52a116f5f5f2
radoslawmotak <radoslaw.motak@seidoit.pl>
2022-08-06 18:41:51 +0200

    Publikacje, nowa wersja;

--- src/app/containers/dictionaries/sit-publications/sit-publications.component.ts
+++ src/app/containers/dictionaries/sit-publications/sit-publications.component.ts
@@ -8,26 +8,43 @@ import { SitDictBaseComponent } from '@app/containers/_base/sit-dict-base/sit-di
   host: {class: 'router-flex'}
 })
 export class SitPublicationsComponent extends SitDictBaseComponent {
+
+  link;
+  ean;
   
   public prepareColumnsDefinitnion(){
     this.gridColumnsDefinition["sitPublications"] = [
       { headerName: 'Id', field: 'sitPublicationsId', type: 'numericColumn', filter: 'agNumberColumnFilter', width: 50, defaultVisibility: false },
       { headerName: 'GUID', field: 'sitPublicationsG', width: 100, defaultVisibility: false },  
-      { headerName: 'Publikacja', field: 'PublicationIdent', filter: 'agTextColumnFilter', width: 300 },
-      { headerName: 'Tytuł', field: 'Title', filter: 'agTextColumnFilter', width: 300 },
-      { headerName: 'Tytuł oryginalny', field: 'OriginalTitle', filter: 'agTextColumnFilter', width: 300 },
+      { headerName: 'Publikacja', field: 'PublicationIdent', filter: 'agTextColumnFilter', width: 350 },
+      { headerName: 'Tytuł', field: 'Title', filter: 'agTextColumnFilter', width: 350 },
       { headerName: 'Status', field: 'StatusValueName', filter: 'agTextColumnFilter', width: 150 },
-      { headerName: 'Temat id', field: 'PublicationSubjectIdent', filter: 'agTextColumnFilter', width: 150, defaultVisibility: false },
-      { headerName: 'Tematyka', field: 'PublicationSubjectName', filter: 'agTextColumnFilter', width: 250 },
-      { headerName: 'Grupa wiekowa', field: 'AudienceName', filter: 'agTextColumnFilter', width: 250, defaultVisibility: false  },
-      { headerName: 'Język oryginału', field: 'LanguageName_Original', filter: 'agTextColumnFilter', width: 150 },
     ];
     this.gridColumnsDefinition["sitPublicationsProducts"] = [
       { headerName: 'Id', field: 'sitPublicationsProductsId', type: 'numericColumn', filter: 'agNumberColumnFilter', width: 50, defaultVisibility: false },
       { headerName: 'GUID', field: 'sitPublicationsProductsG', width: 100, defaultVisibility: false }, 
-      { headerName: 'Identfikator', field: 'ProductIdent', width: 150},
+      { headerName: 'Identfikator', field: 'ProductIdent', width: 130},
+      { headerName: 'EAN', field: 'EAN', filter: 'agTextColumnFilter', width: 120 },
       { headerName: 'Nazwa', field: 'ProductName', width: 300},
-      { headerName: 'Forma wydania', field: 'FormOfReleaseIdent', width: 300}
+      { headerName: 'Status', field: 'StatusValueName_Main', filter: 'agSetColumnFilter', width: 100, floatingFilter: false, 
+        cellStyle: function(params) {
+          if (params.value === 'W przygotowaniu') { return { color: 'violet', 'font-weight': 600 }; }
+          else if (params.value === 'Zapowiedź') { return { color: 'orange', 'font-weight': 600 }; }
+          else if (params.value === 'Nowość') { return { color: 'rgb(153, 0, 0)', 'font-weight': 600 }; }
+          else if (params.value === 'Aktywna') { return { color: 'rgb(20, 152, 46)', 'font-weight': 600 }; }
+          else if (params.value === 'Wyprzedaż') { return { color: 'rgb(11, 23, 255)', 'font-weight': 600 }; }
+          else if (params.value === 'Wycofana') { return { color: 'black', 'font-weight': 600 }; }          
+          else { return null; }
+        }
+      },       
     ];
    }
+
+   activeRowProductsChanged(activeRow) {
+    this.link = activeRow?.sitImagesG == null
+      ? this.urlService.getImageUrl("noimage", "noimage.jpg") // kiedy brak rekordu
+      :  this.urlService.getImageUrl(activeRow.sitImagesG, activeRow.FileName) ;
+
+      this.ean = activeRow !== null ? activeRow.EAN : '';
+  }   
 }
