9985c86339192aee0eadf910ad57f4e59c8f00a2
Piotr Ratajczak <piotr.ratajczak@seidoit.pl>
2022-11-02 17:31:55 +0100

    Modyfikacja skrypu buildującego

--- scripts/deploy/zip.ps1
+++ scripts/deploy/zip.ps1
@@ -4,7 +4,8 @@ $distPath = $args[1];
 $versionPath = $args[2];
 
 # Alias for 7-zip
-if (-not (test-path "C:\Program Files (x86)\7-Zip\7z.exe")) {throw "C:\Program Files (x86)\7-Zip\7z.exe needed"}
+# if (-not (test-path "C:\Program Files (x86)\7-Zip\7z.exe")) {throw "C:\Program Files (x86)\7-Zip\7z.exe needed"}
+if (-not (test-path "C:\Program Files\7-Zip\7z.exe")) {throw "C:\Program Files\7-Zip\7z.exe needed"}
 set-alias sz "C:\Program Files (x86)\7-Zip\7z.exe"
 
 
