efc89103ec63b108d85e24226fae410ca2ee61c4
Piotr Ratajczak <piotr.ratajczak@seidoit.pl>
2023-01-18 15:22:33 +0100

    Version 2.1.254 build

--- src/app/containers/dictionaries/sit-agreements/sit-agreements.component.ts
+++ src/app/containers/dictionaries/sit-agreements/sit-agreements.component.ts
@@ -56,8 +56,6 @@ export class SitAgreementsComponent extends SitDictBaseComponent {
 
   refreshAfter(dataSourceManager) {
     this.sitAgreementCustomers = dataSourceManager?.getDateSourceWrapper("sitAgreementCustomers").rows;
-
-    console.log(this.sitAgreementCustomers)
     
   }  
 }
