4b701b348a3ae195805a02ff2df0199cbb4017be
Jaca <joca2@wp.pl>
2023-02-03 13:22:09 +0100

    sit-data-textarea, sit-agreements, globalny css i inne poprawki
    sit-data-textarea.componen,
    sit-agreements.component,
    globalny css,
    sit-proc-expander.component - prace przygotowawcze,
    inne poprawki

--- src/app/components/controls/sit-data-textarea/sit-data-textarea.component.ts
+++ src/app/components/controls/sit-data-textarea/sit-data-textarea.component.ts
@@ -1,17 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 import { SitDataBaseComponent } from '../sit-data-base/sit-data-base.component';
 import { MatFormFieldAppearance  } from '@angular/material/form-field';
 
 @Component({
   selector: 'sit-data-textarea',
   templateUrl: './sit-data-textarea.component.html',
-  styleUrls: ['./sit-data-textarea.component.scss']
+  styleUrls: ['./sit-data-textarea.component.scss'],
+  host: {class: 'sit-data-textarea-component'},
+  encapsulation : ViewEncapsulation.None,
 })
 export class SitDataTextareaComponent extends SitDataBaseComponent {
   @Input() type = 'text';
   @Input() label = '';
   @Input() appearance: MatFormFieldAppearance = 'legacy';
-  @Input() rows: number;
+  @Input() rows: number = 3;
   @Input() cols: number;
 
   onChange(event: any) {
