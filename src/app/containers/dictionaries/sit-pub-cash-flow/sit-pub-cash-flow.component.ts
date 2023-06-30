092a85b243a036e502b11e36e7ca3c432d4e7240
radoslawmotak <radoslaw.motak@seidoit.pl>
2023-06-30 07:41:32 +0200

    Pivot w Spływie dla Publicat

--- src/app/containers/dictionaries/sit-pub-cash-flow/sit-pub-cash-flow.component.ts
+++ src/app/containers/dictionaries/sit-pub-cash-flow/sit-pub-cash-flow.component.ts
@@ -17,6 +17,11 @@ export class SitPubCashFlowComponent extends SitDictBaseComponent {
   public rowGroupPanelShow: any;
   public suppressAggFuncInHeader: any;
   public groupIncludeTotalFooter: any;
+
+  public pivotDefaultColDef: any;
+  public pivotAutoGroupColumnDef: any;
+  public pivotRowTotals: any;
+  public pivotColumnGroupTotals: any;  
   
   constructor(
     protected gatewayService: GatewayService,
@@ -41,22 +46,32 @@ export class SitPubCashFlowComponent extends SitDictBaseComponent {
       this.rowGroupPanelShow = 'always';
       this.suppressAggFuncInHeader = 'true';
       this.groupIncludeTotalFooter = 'true';
+      //
+      this.pivotDefaultColDef = {
+        flex: 0,
+        minWidth: 120,
+        sortable: true,
+        resizable: true,
+      };
+      this.pivotAutoGroupColumnDef = { 
+        minWidth: 250, 
+        pinned: 'left',
+        sort: 'asc'
+      };
+      this.pivotRowTotals = 'after';
+      this.pivotColumnGroupTotals = 'before';
     };  
 
   public prepareColumnsDefinitnion() {
     this.gridColumnsDefinition["sitCashFlow"] = [
       { headerName: 'Handlowiec', field: 'AccountManager_Payer', filter: 'agTextColumnFilter', width: 200,
-        enableRowGroup: true, rowGroup: true, hide: true
-      },
+        enableRowGroup: true, rowGroup: true, hide: true },
       { headerName: 'Grupa', field: 'CustomersGroupIdent', filter: 'agTextColumnFilter', width: 100, defaultVisibility: false,
-        enableRowGroup: true
-      },
+        enableRowGroup: true },
       { headerName: 'Nazwa grupy', field: 'CustomersGroupName', filter: 'agTextColumnFilter', width: 200,
-        enableRowGroup: true, rowGroup: true, hide: true
-      },
+        enableRowGroup: true, rowGroup: true, hide: true },
       { headerName: 'Płatnik', field: 'AddGroupIdent', filter: 'agTextColumnFilter', width: 100,
-        enableRowGroup: true, rowGroup: true, hide: true
-      },
+        enableRowGroup: true, rowGroup: true, hide: true },
       { headerName: 'Kontrahent', field: 'CustIdent_Payer', filter: 'agTextColumnFilter', minWidth: 100, maxWidth: 150, },
       { headerName: 'Nazwa kontrahenta', field: 'CustName_Payer', filter: 'agTextColumnFilter', width: 100, },
       { headerName: 'Spływ', field: 'DepositAmount', type: 'numericColumn', renderType:'number', suppressMenu: true, width: 100,
@@ -68,6 +83,33 @@ export class SitPubCashFlowComponent extends SitDictBaseComponent {
       { headerName: 'Zwroty', field: 'ReturnAmount', type: 'numericColumn', renderType:'number', suppressMenu: true, width: 100,
         aggFunc: 'sum' },
     ];
+
+    this.gridColumnsDefinition["sitCashFlowPivot"] = [
+      { headerName: 'Handlowiec', field: 'AccountManager_Payer', filter: 'agTextColumnFilter', width: 200,
+        enableRowGroup: true, rowGroup: true, enablePivot: true },
+      { headerName: 'Grupa', field: 'CustomersGroupIdent', filter: 'agTextColumnFilter', width: 100, defaultVisibility: false,
+        enableRowGroup: true },
+      { headerName: 'Nazwa grupy', field: 'CustomersGroupName', filter: 'agTextColumnFilter', width: 200,
+        enableRowGroup: true, rowGroup: true, enablePivot: true },
+      { headerName: 'Płatnik', field: 'AddGroupIdent', filter: 'agTextColumnFilter', width: 100,
+        enableRowGroup: true, rowGroup: true, enablePivot: true },
+      { headerName: 'Kontrahent', field: 'CustIdent_Payer', filter: 'agTextColumnFilter', minWidth: 100, maxWidth: 150, 
+        enableRowGroup: true },
+      { headerName: 'Nazwa kontrahenta', field: 'CustName_Payer', filter: 'agTextColumnFilter', width: 100, 
+        enableRowGroup: true },
+      { headerName: 'Rok', field: 'Year', filter: 'agTextColumnFilter', width: 100,
+        pivot: true, enablePivot: true },  
+      { headerName: 'Rok-miesiąc', field: 'YearMonth', filter: 'agTextColumnFilter', width: 100,
+        pivot: true, enablePivot: true }, 
+      { headerName: 'Spływ', field: 'DepositAmount', type: 'numericColumn', renderType:'number', suppressMenu: true, width: 100,
+        aggFunc: 'sum', enableValue: true },
+      { headerName: 'Sprzedaż', field: 'SaleAmount', type: 'numericColumn', renderType:'number', suppressMenu: true, width: 100,
+        aggFunc: 'sum', enableValue: true },
+      { headerName: 'Korekty', field: 'CorrectionAmount', type: 'numericColumn', renderType:'number', suppressMenu: true, width: 100,
+        aggFunc: 'sum', enableValue: true },
+      { headerName: 'Zwroty', field: 'ReturnAmount', type: 'numericColumn', renderType:'number', suppressMenu: true, width: 100,
+        aggFunc: 'sum', enableValue: true },
+    ];    
   }
 
 }
