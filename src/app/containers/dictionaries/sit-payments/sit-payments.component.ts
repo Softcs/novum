96c33594fcbd0c5b0908baa3e594f47308b1747a
radoslawmotak <radoslaw.motak@seidoit.pl>
2023-07-04 22:09:01 +0200

    Paczki przelewów 02

--- src/app/containers/dictionaries/sit-payments/sit-payments.component.ts
+++ src/app/containers/dictionaries/sit-payments/sit-payments.component.ts
@@ -14,21 +14,44 @@ export class SitPaymentsComponent extends SitDictBaseComponent {
     this.gridColumnsDefinition["sitPayments"] = [
       { headerName: 'Id', field: 'sitOfficeDocHeadersId', type: 'numericColumn', filter: 'agNumberColumnFilter', width: 50, defaultVisibility: false },
       { headerName: 'GUID', field: 'sitOfficeDocHeadersG', width: 100, defaultVisibility: false },
-      { headerName: 'Data płatności', field: 'PaymentDate', width: 120, renderType: 'date', suppressMenu: true },
-      { headerName: 'Kontrahent', field: 'CustIdent', filter: 'agTextColumnFilter', width: 100, suppressMenu: true, defaultVisibility: true },
-      { headerName: 'Nazwa kontrahenta', field: 'CustName', filter: 'agTextColumnFilter', width: 250, defaultVisibility: true },
-      { headerName: 'Numer dokumentu', field: 'DocumentNumber', filter: 'agTextColumnFilter', width: 150, defaultVisibility: true },
-      { headerName: 'Brutto', field: 'GrossCurrency', filter: 'agNumberColumnFilter', type: 'numericColumn', renderType:'number', width: 120, suppressMenu: true, 
-        defaultVisibility: false},
-      { headerName: 'VAT', field: 'VATCurrency', filter: 'agNumberColumnFilter', type: 'numericColumn', renderType:'number', width: 120, suppressMenu: true, 
-        defaultVisibility: false},
-      { headerName: 'Brutto do zapłaty', field: 'GrossToPayment', filter: 'agNumberColumnFilter', type: 'numericColumn', renderType:'number', width: 120, 
-        suppressMenu: true, defaultVisibility: true},
-      { headerName: 'VAT do zapłaty', field: 'VATToPayment', filter: 'agNumberColumnFilter', type: 'numericColumn', renderType:'number', width: 120, suppressMenu: true, 
-        defaultVisibility: true},
+      { headerName: 'Płatność',
+        children: [
+          { headerName: 'Po term', field: 'OverdueDays', type: 'numericColumn', width: 70, suppressMenu: true, defaultVisibility: true, },
+          { headerName: 'Data', field: 'PaymentDate', width: 100, renderType: 'date', suppressMenu: true, defaultVisibility: true, },
+        ]
+      },
+      { headerName: 'Kontrahent',
+        children: [      
+          { headerName: 'Identyfikator', field: 'CustIdent', filter: 'agTextColumnFilter', width: 100, suppressMenu: true, defaultVisibility: true, },
+          { headerName: 'Nazwa', field: 'CustName', filter: 'agTextColumnFilter', width: 250, defaultVisibility: true, },
+        ]
+      },
+      { headerName: 'Dokument',
+        children: [      
+          { headerName: 'Numer', field: 'DocumentNumber', filter: 'agTextColumnFilter', width: 150, defaultVisibility: true },
+          { headerName: 'Brutto', field: 'GrossCurrency', filter: 'agNumberColumnFilter', type: 'numericColumn', renderType:'number', width: 100, suppressMenu: true, 
+            defaultVisibility: false},
+          { headerName: 'VAT', field: 'VATCurrency', filter: 'agNumberColumnFilter', type: 'numericColumn', renderType:'number', width: 100, suppressMenu: true, 
+            defaultVisibility: false},
+        ]
+      },
+      { headerName: 'Do zapłaty',
+        children: [      
+          { headerName: 'Brutto', field: 'GrossToPayment', filter: 'agNumberColumnFilter', type: 'numericColumn', renderType:'number', width: 100, 
+            suppressMenu: true, defaultVisibility: true, cellStyle: function(params) { return {backgroundColor: '#d6f5d6'} },
+            cellClass: ['font12','greenBackground','numberFormatInt'], },
+          { headerName: 'VAT', field: 'VATToPayment', filter: 'agNumberColumnFilter', type: 'numericColumn', renderType:'number', width: 100, 
+            suppressMenu: true, defaultVisibility: true, cellStyle: function(params) { return {backgroundColor: '#d6f5d6'} },
+            cellClass: ['font12','greenBackground','numberFormatInt'], },
+        ]
+      },
       { headerName: 'Waluta', field: 'CurrencyIdent', filter: 'agTextColumnFilter', width: 70, suppressMenu: true, defaultVisibility: true },
-      { headerName: 'Split payment', field: 'IsSplitPayment', filter: 'agSetColumnFilter', maxWidth: 90, renderType: 'checkbox', suppressMenu: true,
-        cellClass: "grid-cell-centered", defaultVisibility: true },   
+      { headerName: 'Split',
+        children: [ 
+          { headerName: 'payment', field: 'IsSplitPayment', filter: 'agSetColumnFilter', maxWidth: 80, renderType: 'checkbox', suppressMenu: true,
+            cellClass: "grid-cell-centered", defaultVisibility: true },   
+        ]
+      },
       { headerName: 'Opis dokumentu', field: 'OfficeDocDescription', filter: 'agTextColumnFilter', width: 300, defaultVisibility: true },
       { headerName: 'Status', field: 'StatusValueName', filter: 'agTextColumnFilter', renderType: 'status', width: 67,  suppressMenu: true, defaultVisibility: false, },      
     ];
@@ -39,7 +62,7 @@ export class SitPaymentsComponent extends SitDictBaseComponent {
       { headerName: 'Data paczki', field: 'BankTransferDate', width: 120, renderType: 'date', suppressMenu: true },
       { headerName: 'Data płatności', field: 'BankTransferPaymentDate', width: 120, renderType: 'date', suppressMenu: true },
       { headerName: 'Numer paczki', field: 'BankTransferNumber', filter: 'agTextColumnFilter', width: 150, defaultVisibility: true },
-      { headerName: 'Konto bankowe', field: 'BankAccount', filter: 'agTextColumnFilter', width: 150, defaultVisibility: true },
+      { headerName: 'Konto bankowe', field: 'BankAccount', filter: 'agTextColumnFilter', width: 210, defaultVisibility: true },
       { headerName: 'Wal.', field: 'CurrencyIdent', tooltipField: 'CurrencyIdent', filter: 'agTextColumnFilter', width: 50,  suppressMenu: true, },
       { headerName: 'Opis paczki', field: 'Description', filter: 'agTextColumnFilter', width: 300, defaultVisibility: true },
     ];
@@ -49,8 +72,8 @@ export class SitPaymentsComponent extends SitDictBaseComponent {
       { headerName: 'GUID', field: 'sitBankTransferPositionsG', width: 100, defaultVisibility: false },
       { headerName: 'Lp', field: 'OrdNumber', type: 'numericColumn', sortable: true, resizable: true, suppressMenu: true, width: 50 },
       { headerName: 'Kontrahent', field: 'CustomerInfo', filter: 'agTextColumnFilter', width: 150, defaultVisibility: true },
-      { headerName: 'ID kontrahenta', field: 'CustomerId', filter: 'agTextColumnFilter', width: 150, defaultVisibility: true },
-      { headerName: 'Konto bankowe', field: 'CustomerBankAccount', filter: 'agTextColumnFilter', width: 150, defaultVisibility: true },
+      { headerName: 'ID kontrahenta', field: 'CustomerID', filter: 'agTextColumnFilter', width: 150, defaultVisibility: true },
+      { headerName: 'Konto bankowe', field: 'CustomerBankAccount', filter: 'agTextColumnFilter', width: 210, defaultVisibility: true },
       { headerName: 'Opis pozycji', field: 'PositionDescription', filter: 'agTextColumnFilter', width: 300, defaultVisibility: true },
       { headerName: 'Nr faktury', field: 'InvoiceNumber', filter: 'agTextColumnFilter', width: 150, defaultVisibility: true },
       { headerName: 'Brutto faktury', field: 'GrossCurrency', filter: 'agNumberColumnFilter', type: 'numericColumn', renderType:'number', width: 120, 
