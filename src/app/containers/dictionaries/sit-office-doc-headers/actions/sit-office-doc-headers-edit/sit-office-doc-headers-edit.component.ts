a754092b884910ddf9ed0c5ce95f3af2df3743cf
Jaca <joca2@wp.pl>
2023-07-13 17:49:40 +0200

    fxFlex: sit-proc-expander, sit-proc-params, sit-office-doc-headers-edit, sit-pdf-viewer + scss-y

--- src/app/containers/dictionaries/sit-office-doc-headers/actions/sit-office-doc-headers-edit/sit-office-doc-headers-edit.component.ts
+++ src/app/containers/dictionaries/sit-office-doc-headers/actions/sit-office-doc-headers-edit/sit-office-doc-headers-edit.component.ts
@@ -1,24 +1,31 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { SitActionParamsForm } from '@app/_interfaces/sitActionParamsForm';
 
 @Component({
   selector: 'app-sit-office-doc-headers-edit',
   templateUrl: './sit-office-doc-headers-edit.component.html',
   styleUrls: ['./sit-office-doc-headers-edit.component.scss'],
-  host: {class: 'sit-office-doc-headers-edit-component'},
+  host: {class: 'sit-office-doc-headers-edit-component flex-container-column'},
   encapsulation : ViewEncapsulation.None,
 })
 export class SitOfficeDocHeadersEditComponent extends SitActionParamsForm{
 
-  isCurrency(){
+
+  showPDF: boolean = false;
+
+  showPDFChangeView = (val: boolean) => {
+    this.showPDF = val;
+  }
+
+  isCurrency () {
     return( !this.activeRow?.sitCurrenciesG || this.activeRow?.sitCurrenciesG == 'E43EB905-3B49-432B-AD42-0D7E2618C82D' ? false : true );
   }
 
-  hideNewCustomer(){
+  hideNewCustomer () {
     return(!this.activeRow  || this.activeRow.CustomerInsert == 0 ? true : false);
   }
 
-  bankNoReq(){
+  bankNoReq () {
     return(!this.activeRow  || this.activeRow.BankAccountNumberReq == 0 ? false : true);
 
   }
