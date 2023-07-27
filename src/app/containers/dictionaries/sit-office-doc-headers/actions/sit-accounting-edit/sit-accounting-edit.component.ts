5ac51fc9e7f99ee6e46374c51441c91455053307
Jaca <joca2@wp.pl>
2023-07-27 12:57:21 +0200

    kancelaria - porzadki + fx=out
    #100

--- src/app/containers/dictionaries/sit-office-doc-headers/actions/sit-accounting-edit/sit-accounting-edit.component.ts
+++ src/app/containers/dictionaries/sit-office-doc-headers/actions/sit-accounting-edit/sit-accounting-edit.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { SitActionParamsForm } from '@app/_interfaces/sitActionParamsForm';
 
 @Component({
   selector: 'app-sit-accounting-edit',
   templateUrl: './sit-accounting-edit.component.html',
   styleUrls: ['./sit-accounting-edit.component.scss'],
-  host: {class: 'sit-accounting-edit-component'}
+  host: {class: 'sit-accounting-edit-component flex-container-column'},
+  encapsulation : ViewEncapsulation.None,
 })
 export class SitAccountingEditComponent extends SitActionParamsForm{
 
